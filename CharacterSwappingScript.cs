8a71921c3577c10afbdffc0bacb411cbb025625e
Samuil Petrov <samuil.petrov@maxe-del.de>
2025-12-26 18:43:21 +0100

    feat: Add DTO for RPCs and sync after swap
    When switching characters, the game resets a lot of attributes like health, animations and camera position. Consequently, It is important to store those values and reapply them after switching.
    I started off with a few variables in SwapCharachter() but that quickly became extremely inconsistent and hard to manage.
    Thus, I opted for the PlayerState record with its helper functions to save and apply snapshots of RPCs.

--- CharacterSwappingScript.cs
+++ CharacterSwappingScript.cs
@@ -51,6 +51,67 @@ public class CharacterSwappingScript : Script
     }
 
     /// <summary>
+    /// Represents an immutable Data Transfer Object of a player's state, including position, health and animations.
+    /// </summary>
+    /// <remarks>Use this record to snapshot and restore a player's state when switching characters.
+    /// All values are intended to be consistent with the game world at the time of capture.</remarks>
+    private sealed record PlayerState(
+        GameObject.FVector RpcLoc, GameObject.FRotator RpcRot,
+        GameObject.FVector RppLoc, GameObject.FRotator RppRot,
+        int Health,
+        int BallisticArmour, int MeleeArmour,
+        int CWBallisticArmour, int CWMeleeArmour
+    )
+    {
+        /// <summary>
+        /// Creates a new DTO based on the specified RPC and persistent data.
+        /// </summary>
+        /// <remarks>Should be used before <see cref="DoSwitch(WorldInfo, CharacterInfo, RPawnPlayer, RPlayerController)">
+        /// to capture the RPC state right before switching characters.</remarks>
+        /// <param name="rpc">The RPC to snapshot before the character switch.</param>
+        /// <param name="pData">The persistent data object containing player health and armor values.</param>
+        /// <returns>A PlayerState snapshot of the player</returns>
+        public static PlayerState FromRpc(RPlayerController rpc, RPersistentData pData)
+        {
+            var rpp = rpc.CombatPawn;
+            return new PlayerState(
+                rpc.Location, rpc.Rotation,
+                rpp.Location, rpp.Rotation,
+                pData.PlayerHealth,
+                pData.BallisticArmour, pData.MeleeArmour,
+                pData.CWBallisticArmour, pData.CWMeleeArmour
+            );
+        }
+
+        /// <summary>
+        /// Applies the values of the DTO (before the character switch) to the RPC (after the switch),
+        /// therefore, allowing for smoother transitions.
+        /// </summary>
+        /// <param name="rpc">The RPC to restore the state of</param>
+        /// <param name="pData">The persistent data object containing player health and armor values.</param>
+        public void ApplyToRpc(RPlayerController rpc, RPersistentData pData)
+        {
+            var rpp = rpc.CombatPawn;
+            // RSeqAct_SwitchPlayerCharacter isn't reliable in Challenge Maps,
+            // therefore, we must manually override the position
+            rpp.SetLocation(RppLoc);
+            rpp.SetRotation(RppRot);
+            // Makes sure the camera doesn't change with the character swap
+            rpc.SetLocation(RpcLoc);
+            rpc.SetRotation(RpcRot);
+            // Transfer health
+            pData.PlayerHealth = Health;
+            pData.BallisticArmour = BallisticArmour;
+            pData.MeleeArmour = MeleeArmour;
+            pData.CWBallisticArmour = CWBallisticArmour;
+            pData.CWMeleeArmour = CWMeleeArmour;
+            //pData.Armo
+            rpp.LoadHealth();
+            // Transfer movement state
+        }
+    }
+
+    /// <summary>
     /// Provides a read-only mapping of each playable character to its associated character information.
     /// </summary>
     /// <remarks>The dictionary contains predefined entries for all supported playable characters. The
@@ -151,10 +212,16 @@ public class CharacterSwappingScript : Script
         var charInfo = Characters[character];
         if (rpp.CharacterName == charInfo.CharacterName) return;
 
+        // Save data that should survive player reinstantiation
+        var dto = PlayerState.FromRpc(rpc, pData);
+
         // Load assets
         LoadPackages(charInfo, gi);
 
         DoSwitch(wi, charInfo, rpp, rpc);
+
+        // Fix inconsistencies after player switch
+        dto.ApplyToRpc(rpc, pData);
 
         // Apply swapping cooldown
         swapCooldownTimer = SwapCooldown;
