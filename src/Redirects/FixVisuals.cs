c6d1d84b26d8f35bf202c62b2732b12062aed5d6
Samuil Petrov <samuil.petrov@maxe-del.de>
2026-04-25 01:24:58 +0200

    fix: Catwoman jewlery showing to Nightwing
    Nightwing also uses a "Thermal" detective mode which causes some internal checks for visuals to detect him as Catwoman.

---
+++ src/Redirects/FixVisuals.cs
@@ -0,0 +1,21 @@
+using BmSDK.BmGame;
+using static Samuil1337.CharacterSwapping.Data.CharacterRegistry;
+
+namespace Samuil1337.CharacterSwapping.Redirects
+{
+    static class FixVisuals
+    {
+        // TODO: Modify RPawnVillain.SetInXrayMode() directly when redirects patched
+        [Redirect(typeof(RPersistentData), nameof(RPersistentData.FinishedCatwomanJewellery))]
+        static bool FinishedCatwomanJewellery(RPersistentData self)
+        {
+            var catwomanName = Characters[Data.PlayableCharacter.Catwoman].CharacterName;
+            if (Game.GetPlayerPawn().CharacterName != catwomanName)
+            {
+                return true;
+            }
+
+            return self.NumJewelleryFound == 16;
+        }
+    }
+}
