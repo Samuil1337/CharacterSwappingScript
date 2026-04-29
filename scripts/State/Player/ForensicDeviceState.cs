Samuil1337/CharacterSwappingScript

commit 490f21bdeb17f321643006893ecf7ff434e42d45

refactor: modularize PlayerState record

scripts/State/Player/ForensicDeviceState.cs
@@ -1,0 +1,48 @@
+using BmSDK.BmGame;
+using BmSDK.Engine;
+
+namespace Samuil1337.CharacterSwapping.State.Player
+{
+    sealed class ForensicDeviceState : IStateComponent
+    {
+        bool _isInvestigateMode;
+        Actor[]? _jammingActors;
+
+        public void CaptureState(SwitchContext ctx)
+        {
+            var rpc = ctx.Rpc;
+            _isInvestigateMode = rpc.bInvestigateMode;
+            _jammingActors = rpc.CurrentForensicsDevice?.JammingActors?.ToArray();
+        }
+
+        public void ApplyState(SwitchContext ctx)
+        {
+            var rpc = ctx.Rpc;
+            rpc.bInvestigateMode = _isInvestigateMode;
+
+            if (rpc.CurrentForensicsDevice is null)
+            {
+                return;
+            }
+
+            var device = rpc.CurrentForensicsDevice;
+            if (_jammingActors is not null)
+            {
+                device.JammingActors = [.. _jammingActors];
+            }
+            else
+            {
+                foreach (var villain in Game.FindObjects<RPawnVillain>())
+                {
+                    if (!villain.IsValid || villain.IsClassDefaultObject)
+                    {
+                        continue;
+                    }
+
+                    // Method ensures that the actor is actually a jammer
+                    device.AddActorToJammerList(villain);
+                }
+            }
+        }
+    }
+}

scripts/State/GameState.cs
@@ -1,0 +1,42 @@
+using Samuil1337.CharacterSwapping.State.Player;
+
+namespace Samuil1337.CharacterSwapping.State
+{
+    interface IStateComponent
+    {
+        void CaptureState(SwitchContext ctx);
+        void ApplyState(SwitchContext ctx);
+    }
+
+    /// <summary>
+    /// Represents an immutable Data Transfer Object of a player's state, including position, health and animations.
+    /// Use this record to snapshot and restore a player's state when switching characters.
+    /// </summary>
+    sealed class GameState : IStateComponent
+    {
+        readonly List<IStateComponent> _stateComponents =
+        [
+            new MovementState(),
+            new HealthState(),
+            new ForensicDeviceState(),
+        ];
+
+        internal GameState(SwitchContext ctx) => CaptureState(ctx);
+
+        public void CaptureState(SwitchContext ctx)
+        {
+            foreach (var component in _stateComponents)
+            {
+                component.CaptureState(ctx);
+            }
+        }
+
+        public void ApplyState(SwitchContext ctx)
+        {
+            foreach (var component in _stateComponents)
+            {
+                component.ApplyState(ctx);
+            }
+        }
+    }
+}

scripts/State/Player/HealthState.cs
@@ -1,0 +1,55 @@
+using static BmSDK.BmGame.RPawnPlayer;
+
+namespace Samuil1337.CharacterSwapping.State.Player
+{
+    sealed class HealthState : IStateComponent
+    {
+        int _health;
+        int _meleeArmor;
+        int _ballisticArmor;
+        int _cwMeleeArmor;
+        int _cwBallisticArmor;
+
+        public void CaptureState(SwitchContext ctx)
+        {
+            // Sync data from RPP to PData
+            var rpp = ctx.Rpp;
+            rpp.HealthUpdated();
+            rpp.SetPersistentMeleeArmour(rpp.CurrentArmourLevels[(int)EArmourType.EA_ArmourMelee]);
+            rpp.SetPersistentBallisticArmour(
+                rpp.CurrentArmourLevels[(int)EArmourType.EA_ArmourBallistic]
+            );
+
+            var pData = ctx.PData;
+            _health = pData.PlayerHealth;
+            _meleeArmor = pData.MeleeArmour;
+            _ballisticArmor = pData.BallisticArmour;
+            _cwMeleeArmor = pData.CWMeleeArmour;
+            _cwBallisticArmor = pData.CWBallisticArmour;
+        }
+
+        public void ApplyState(SwitchContext ctx)
+        {
+            // Transfer health in save data
+            var pData = ctx.PData;
+            pData.PlayerHealth = _health;
+            pData.MeleeArmour = _meleeArmor;
+            pData.BallisticArmour = _ballisticArmor;
+            pData.CWMeleeArmour = _cwMeleeArmor;
+            pData.CWBallisticArmour = _cwBallisticArmor;
+
+            // Apply to pawn
+            var rpp = ctx.Rpp;
+            rpp.Health = _health;
+            rpp.HealthUpdated();
+            rpp.SetArmourCurrent(EArmourType.EA_ArmourMelee, rpp.GetPersistentMeleeArmour());
+            rpp.SetArmourCurrent(
+                EArmourType.EA_ArmourBallistic,
+                rpp.GetPersistentBallisticArmour()
+            );
+
+            // Refresh HUD
+            ctx.Rpc.ShowHealthBar(ctx.Rpc.HudMovieSide);
+        }
+    }
+}

scripts/State/Player/MovementState.cs
@@ -1,0 +1,29 @@
+using System.Numerics;
+using BmSDK;
+
+namespace Samuil1337.CharacterSwapping.State.Player
+{
+    sealed class MovementState : IStateComponent
+    {
+        Vector3 _rpcLoc;
+        Vector3 _rppLoc;
+        Rotator _rpcRot;
+        Rotator _rppRot;
+
+        public void CaptureState(SwitchContext ctx)
+        {
+            _rpcLoc = ctx.Rpc.Location;
+            _rppLoc = ctx.Rpp.Location;
+            _rpcRot = ctx.Rpc.Rotation;
+            _rppRot = ctx.Rpp.Rotation;
+        }
+
+        public void ApplyState(SwitchContext ctx)
+        {
+            ctx.Rpc.SetLocation(_rpcLoc);
+            ctx.Rpp.SetLocation(_rppLoc);
+            ctx.Rpc.SetRotation(_rpcRot);
+            ctx.Rpp.SetRotation(_rppRot);
+        }
+    }
+}

scripts/State/PlayerState.cs
@@ -1,137 +1,0 @@
-using System.Numerics;
-using BmSDK;
-using BmSDK.BmGame;
-using BmSDK.Engine;
-using static BmSDK.BmGame.RPawnPlayer;
-
-namespace Samuil1337.CharacterSwapping.State
-{
-    /// <summary>
-    /// Represents an immutable Data Transfer Object of a player's state, including position, health and animations.
-    /// Use this record to snapshot and restore a player's state when switching characters.
-    /// </summary>
-    sealed record PlayerState(
-        // Camera position
-        Vector3 RpcLoc,
-        Rotator RpcRot,
-        // Character position
-        Vector3 RppLoc,
-        Rotator RppRot,
-        // Base health for all characters
-        int Health,
-        // Batman, Robin and Nightwing armour
-        int MeleeArmor,
-        int BallisticArmor,
-        // Catwoman armour
-        int CwMeleeArmor,
-        int CwBallisticArmor,
-        // Detective vision
-        bool XRay,
-        Actor[]? JammingActors
-    )
-    {
-        /// <summary>
-        /// Creates a new DTO based on the specified RPC and persistent data. Can be applied
-        /// after a switch using <see cref="ApplyToGameState(RPlayerController, RPersistentData)"/>
-        /// </summary>
-        /// <param name="rpc">Controller to snapshot before the character switch.</param>
-        /// <param name="pData">PersistentData to snapshot because some data is invalidated</param>
-        /// <returns>Snapshot of the player before the switch</returns>
-        public static PlayerState FromGameState(RPlayerController rpc, RPersistentData pData)
-        {
-            var rpp =
-                rpc.CombatPawn
-                ?? throw new InvalidOperationException("Controller must possess a pawn");
-
-            // Update persistent health
-            rpp.HealthUpdated();
-            rpp.SetPersistentMeleeArmour(rpp.CurrentArmourLevels[(int)EArmourType.EA_ArmourMelee]);
-            rpp.SetPersistentBallisticArmour(
-                rpp.CurrentArmourLevels[(int)EArmourType.EA_ArmourBallistic]
-            );
-
-            return new PlayerState(
-                RpcLoc: rpc.Location,
-                RpcRot: rpc.Rotation,
-                RppLoc: rpp.Location,
-                RppRot: rpp.Rotation,
-                Health: pData.PlayerHealth,
-                MeleeArmor: pData.MeleeArmour,
-                BallisticArmor: pData.BallisticArmour,
-                CwMeleeArmor: pData.CWMeleeArmour,
-                CwBallisticArmor: pData.CWBallisticArmour,
-                XRay: rpc.bInvestigateMode,
-                JammingActors: rpc.CurrentForensicsDevice?.JammingActors?.ToArray()
-            );
-        }
-
-        /// <summary>
-        /// Applies the values of the DTO (before the character switch) to the RPC (after the switch),
-        /// therefore, allowing for smoother transitions.
-        /// </summary>
-        /// <param name="rpc">The RPC to restore the state of</param>
-        /// <param name="pData">The persistent data object containing player health and armor values.</param>
-        public void ApplyToGameState(RPlayerController rpc, RPersistentData pData)
-        {
-            var rpp = rpc.CombatPawn;
-
-            ApplyMovement(rpc, rpp);
-            ApplyHealth(rpc, rpp, pData);
-            ApplyDetectiveVision(rpc);
-        }
-
-        void ApplyMovement(RPlayerController rpc, RPawnPlayer rpp)
-        {
-            // RSeqAct_SwitchPlayerCharacter isn't reliable in Challenge Maps,
-            // therefore, we must manually override the position
-            rpp.SetLocation(RppLoc);
-            rpp.SetRotation(RppRot);
-            // Makes sure the camera doesn't change with the character swap
-            rpc.SetLocation(RpcLoc);
-            rpc.SetRotation(RpcRot);
-        }
-
-        void ApplyHealth(RPlayerController rpc, RPawnPlayer rpp, RPersistentData pData)
-        {
-            // Transfer health in save data
-            pData.PlayerHealth = Health;
-            pData.BallisticArmour = BallisticArmor;
-            pData.MeleeArmour = MeleeArmor;
-            pData.CWBallisticArmour = CwBallisticArmor;
-            pData.CWMeleeArmour = CwMeleeArmor;
-
-            // Apply to pawn
-            rpp.Health = Health;
-            rpp.HealthUpdated();
-            rpp.SetArmourCurrent(EArmourType.EA_ArmourMelee, rpp.GetPersistentMeleeArmour());
-            rpp.SetArmourCurrent(
-                EArmourType.EA_ArmourBallistic,
-                rpp.GetPersistentBallisticArmour()
-            );
-
-            // Refresh HUD
-            rpc.ShowHealthBar(rpc.HudMovieSide);
-        }
-
-        void ApplyDetectiveVision(RPlayerController rpc)
-        {
-            if (rpc.CurrentForensicsDevice is null)
-            {
-                return;
-            }
-
-            // Add Jammers
-            var device = rpc.CurrentForensicsDevice;
-            if (JammingActors is not null)
-            {
-                device.JammingActors = [.. JammingActors];
-            }
-
-            // Transfer Detective Mode state
-            if (device.bCanUseForensicsDeviceDirectly)
-            {
-                rpc.bInvestigateMode = XRay;
-            }
-        }
-    }
-}

scripts/State/SwitchContext.cs
@@ -65,10 +65,10 @@ namespace Samuil1337.CharacterSwapping.State
                 return false;
             }
 
-            var dto = PlayerState.FromGameState(Rpc, PData);
+            var dto = new GameState(this);
             LoadAssets();
             DoSwitch();
-            dto.ApplyToGameState(Rpc, PData);
+            dto.ApplyState(this);
 
             if (_effectTemplate is not null)
             {

commit eade449206372c5e2bee59d418a77ca8b7226ac7

fix: xray reenabling after switching from bruce
This fixes a regression from 490f21bdeb17f321643006893ecf7ff434e42d45

scripts/State/Player/ForensicDeviceState.cs
@@ -18,8 +18,6 @@ namespace Samuil1337.CharacterSwapping.State.Player
         public void ApplyState(SwitchContext ctx)
         {
             var rpc = ctx.Rpc;
-            rpc.bInvestigateMode = _isInvestigateMode;
-
             if (rpc.CurrentForensicsDevice is null)
             {
                 return;
@@ -42,6 +40,11 @@ namespace Samuil1337.CharacterSwapping.State.Player
                     // Method ensures that the actor is actually a jammer
                     device.AddActorToJammerList(villain);
                 }
+            }
+
+            if (device.bCanUseForensicsDeviceDirectly)
+            {
+                rpc.bInvestigateMode = _isInvestigateMode;
             }
         }
     }