e2f828d59f16d59eb68d0176414e12a909e8c01d
Samuil Petrov <samuil.petrov@maxe-del.de>
2026-05-01 15:01:26 +0200

    feat: regenerate Nigthwing Wrist Dart gadget
    Nightwing's Wrist Darts are programmed only for Challenge maps.
    I have extended the code to replicate the default regeneration of RProjectileGadgetBase:
    Inside of buildings, we regenerate on room change.
    Outside, we regenerate every 30 seconds.

---
+++ scripts/Patches/RegenWristDart.cs
@@ -0,0 +1,95 @@
+using System.Numerics;
+using BmSDK;
+using BmSDK.BmGame;
+using BmSDK.BmScript;
+
+namespace Samuil1337.CharacterSwapping.Patches
+{
+    [ScriptComponent(AutoAttach = true)]
+    sealed class RestockWristDartComponent : ScriptComponent<RNightwingWristDart>
+    {
+        static readonly float s_rechargeTime = RProjectileGadgetBase.DefaultObject.ReplenishTime;
+        bool _replenishing;
+        float _cooldown;
+
+        [ComponentRedirect(nameof(RNightwingWristDart.FireDart))]
+        void FireDart(Rotator rotation, Vector3 position)
+        {
+            if (Owner.Ammo > 0)
+            {
+                Owner.FireDart(rotation, position);
+                OnDecrementAmmo();
+            }
+        }
+
+        [ComponentRedirect(nameof(RNightwingWristDart.QuickFireDart))]
+        void QuickFireDart()
+        {
+            if (Owner.QuickFireTarget is not null)
+            {
+                Owner.QuickFireDart();
+                OnDecrementAmmo();
+            }
+        }
+
+        void OnDecrementAmmo()
+        {
+            if (Game.GetGameRI().IsOverworldGameplay())
+            {
+                if (!_replenishing)
+                {
+                    ScheduleIncrementAmmo();
+                }
+            }
+        }
+
+        void ScheduleIncrementAmmo()
+        {
+            _replenishing = true;
+            _cooldown = s_rechargeTime;
+        }
+
+        public override void OnTick()
+        {
+            if (_replenishing)
+            {
+                _cooldown -= Game.GetDeltaTime();
+                if (_cooldown <= 0)
+                {
+                    _replenishing = false;
+                    IncrementAmmo();
+                }
+            }
+        }
+
+        void IncrementAmmo()
+        {
+            if (Owner.Ammo < Owner.MaxAmmo)
+            {
+                Owner.Ammo++;
+                Owner.UpdateGadgetHUDParams();
+                if (Owner.Ammo < Owner.MaxAmmo)
+                {
+                    ScheduleIncrementAmmo();
+                }
+            }
+        }
+
+        [ComponentRedirect(nameof(RNightwingWristDart.OnRoomChange))]
+        void OnRoomChange()
+        {
+            if (!Game.GetGameRI().IsOverworldGameplay())
+            {
+                Owner.RestockAmmo();
+            }
+        }
+
+        [ComponentRedirect(nameof(RNightwingWristDart.RestockAmmo))]
+        void RestockAmmo()
+        {
+            Owner.Ammo = Owner.MaxAmmo;
+            Owner.NumHeadShotsInRound = 0;
+            Owner.UpdateGadgetHUDParams();
+        }
+    }
+}
