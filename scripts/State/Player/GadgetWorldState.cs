ea4fc8d0f35b5c5dcf30bd62a291841df0c3762f
Samuil Petrov <samuil.petrov@maxe-del.de>
2026-05-26 12:26:35 +0200

    feat: do not despawn projectiles on switch

--- scripts/State/Player/GadgetWorldState.cs
+++ scripts/State/Player/GadgetWorldState.cs
@@ -4,19 +4,27 @@ namespace Samuil1337.CharacterSwapping.State
 {
     sealed class GadgetWorldState : IStateComponent
     {
+        RProjectile[] _projectiles = [];
         RFloatingRaft[] _rafts = [];
 
         public void CaptureState(SwitchContext ctx)
         {
-            _rafts = [.. ctx.Rpp.IceRafts];
+            var rpp = ctx.Rpp;
+
+            _projectiles = [.. rpp.SpawnedProjectiles];
+            rpp.SpawnedProjectiles.Clear();
+            _rafts = [.. rpp.IceRafts];
         }
 
         public void ApplyState(SwitchContext ctx)
         {
-            ctx.Rpp.IceRafts = [.. _rafts];
+            var rpp = ctx.Rpp;
+
+            rpp.SpawnedProjectiles.CopyFrom(_projectiles);
+            rpp.IceRafts = [.. _rafts];
             foreach (var raft in _rafts)
             {
-                raft.Player = ctx.Rpp;
+                raft.Player = rpp;
             }
         }
     }
