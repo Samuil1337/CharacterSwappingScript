a064b539c80a2b58b8cb38f430fce766ddae63fd
Samuil Petrov <samuil.petrov@maxe-del.de>
2026-05-30 22:08:39 +0200

    fix: HQR Robin health inconsistent

--- scripts/Patches/Health/ForceUseSavedArmor.cs
+++ scripts/Patches/Health/ForceUseSavedArmor.cs
@@ -25,6 +25,28 @@ namespace Samuil1337.CharacterSwapping.Patches
     }
 
     /// <summary>
+    /// HQR Robin overrides the functions which manage saved armor. However, in contrast
+    /// to Batman, he stores the data in ProgressCharacterStatus. To match the other characters
+    /// and prevent inconsistencies we override this behavior ourselves.
+    /// </summary>
+    [ScriptComponent(AutoAttach = true)]
+    sealed class ForceDlcRobinUseSavedArmor : ScriptComponent<RPawnPlayerRobinStoryDLC>
+    {
+        [ComponentRedirect(nameof(RPawnPlayerRobinStoryDLC.GetPersistentMeleeArmour))]
+        int GetPersistentMeleeArmour() => Game.GetPersistentData().MeleeArmour;
+
+        [ComponentRedirect(nameof(RPawnPlayerRobinStoryDLC.GetPersistentBallisticArmour))]
+        int GetPersistentBallisticArmour() => Game.GetPersistentData().BallisticArmour;
+
+        [ComponentRedirect(nameof(RPawnPlayerRobinStoryDLC.SetPersistentMeleeArmour))]
+        void SetPersistentMeleeArmour(int armor) => Game.GetPersistentData().MeleeArmour = armor;
+
+        [ComponentRedirect(nameof(RPawnPlayerRobinStoryDLC.SetPersistentBallisticArmour))]
+        void SetPersistentBallisticArmour(int armor) =>
+            Game.GetPersistentData().BallisticArmour = armor;
+    }
+
+    /// <summary>
     /// Nightwing doesn't override the functions which manage saved armor.
     /// This makes them noop, breaking health transfer and regeneration logic.
     /// Therefore, we override the behavior ourselves.
