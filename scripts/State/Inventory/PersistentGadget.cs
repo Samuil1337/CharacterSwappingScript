ad11670dfa866d2aa93fbcae3fa2f062503dc783
Samuil Petrov <samuil.petrov@maxe-del.de>
2026-06-01 16:43:22 +0200

    refactor: move PersistentJammer to own file

--- scripts/State/Inventory/PersistentGadget.cs
+++ scripts/State/Inventory/PersistentGadget.cs
@@ -84,45 +84,4 @@ namespace Samuil1337.CharacterSwapping.State
 
         public void RestockAmmo() => Ammo = MaxAmmo;
     }
-
-    sealed class PersistentJammer(PersistentInventory inventory)
-        : PersistentGadget<RJammerGadget>(inventory)
-    {
-        protected override RJammerGadget? GetGadget() => Inventory.Owner?.CombatPawn?.JammerGadget;
-
-        public override void CaptureState()
-        {
-            var jammer = GetGadget()!;
-            MaxAmmo = jammer.MaxAmmo;
-            Ammo = jammer.Ammo;
-            ReplenishTime = jammer.ReplenishTime;
-            CurrentReplenishTime = jammer.CurrentRechargeTime;
-        }
-
-        public override void ApplyState(bool isOverworld)
-        {
-            // Gadgets are instantiated with max ammo; nothing to do
-            if (Ammo >= MaxAmmo)
-            {
-                return;
-            }
-
-            var jammer = GetGadget()!;
-            jammer.Ammo = Ammo;
-            jammer.UpdateGadgetHUDParams();
-
-            if (isOverworld)
-            {
-                jammer.InterThrowRechargeTime = ReplenishTime;
-                jammer.CurrentRechargeTime = CurrentReplenishTime;
-                jammer.SetTickIsDisabled(false);
-                jammer.SetTimer(CurrentReplenishTime, false, "ReplenishAmmo");
-            }
-            else
-            {
-                jammer.InterThrowRechargeTime = -1;
-                jammer.CurrentRechargeTime = 0;
-            }
-        }
-    }
 }
---
+++ scripts/State/Inventory/PersistentJammer.cs
@@ -0,0 +1,45 @@
+using BmSDK.BmGame;
+
+namespace Samuil1337.CharacterSwapping.State
+{
+    sealed class PersistentJammer(PersistentInventory inventory)
+        : PersistentGadget<RJammerGadget>(inventory)
+    {
+        protected override RJammerGadget? GetGadget() => Inventory.Owner?.CombatPawn?.JammerGadget;
+
+        public override void CaptureState()
+        {
+            var jammer = GetGadget()!;
+            MaxAmmo = jammer.MaxAmmo;
+            Ammo = jammer.Ammo;
+            ReplenishTime = jammer.ReplenishTime;
+            CurrentReplenishTime = jammer.CurrentRechargeTime;
+        }
+
+        public override void ApplyState(bool isOverworld)
+        {
+            // Gadgets are instantiated with max ammo; nothing to do
+            if (Ammo >= MaxAmmo)
+            {
+                return;
+            }
+
+            var jammer = GetGadget()!;
+            jammer.Ammo = Ammo;
+            jammer.UpdateGadgetHUDParams();
+
+            if (isOverworld)
+            {
+                jammer.InterThrowRechargeTime = ReplenishTime;
+                jammer.CurrentRechargeTime = CurrentReplenishTime;
+                jammer.SetTickIsDisabled(false);
+                jammer.SetTimer(CurrentReplenishTime, false, "ReplenishAmmo");
+            }
+            else
+            {
+                jammer.InterThrowRechargeTime = -1;
+                jammer.CurrentRechargeTime = 0;
+            }
+        }
+    }
+}
